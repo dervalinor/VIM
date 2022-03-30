8e67474f476edb0ca802ae13ce39b5f9c31b7550
melkorval <prushka@tutanota.com>
2022-03-30 10:35:59 -0500

    How to open new line and mode visual

--- 4_insert/index.js
+++ 4_insert/index.js
@@ -9,7 +9,7 @@ const suma = (ns) => {
   return acumulado;
 } 
 
-//hello hello hello
+//holaa hello hello
 const numeros = [1, 2, 3, 4, 5];
 // multiplicar * 2
 const multiplicados = numeros.map(x => x * 2)
